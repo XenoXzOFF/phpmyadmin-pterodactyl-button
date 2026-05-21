565e46bc454dd8061ed88b839f96c44a0d6d4932
XenoXzOFF <xenoxzperso@gmail.com>
2026-05-21 14:48:58 +0200

    Création

---
+++ setup.sh
@@ -0,0 +1,28 @@
+cd /var/www/pterodactyl/resources/scripts/components/server/databases
+rm -rf DatabaseRow.tsx
+wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/phpmyadmin/DatabaseRow.tsx
+cd /var/www/pterodactyl/public
+rm -rf pma_redirect.html
+wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/phpmyadmin/pma_redirect.html
+if [ -z "$pmalocation" ]; then
+#clear
+echo "phpmyadmin already installed"
+else
+#clear
+echo " "
+echo 'Where is phpMyAdmin located? Make sure to have "http://" or "https://" !'
+echo " "
+echo "For example:"
+echo "https://pma.yourdomain.com"
+echo "OR"
+echo "https://panel.yourdomain.com/phpmyadmin"
+echo " "
+read pmalocation
+sed -i "s|http:\/\/yourdomain.com\/phpmyadmin|$pmalocation|g" /var/www/pterodactyl/public/pma_redirect.html
+fi
+#clear
+cd /var/www/pterodactyl
+yarn run build:production
+#clear
+php /var/www/pterodactyl/artisan up
+echo "phpMyAdmin successfully installed"
